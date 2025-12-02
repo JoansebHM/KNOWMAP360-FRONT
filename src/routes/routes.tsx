f08e4e2540b9984184ce505737226e5aaa7f2360
Sebastian Herrera <sebashm621@gmail.com>
2025-12-02 12:43:03 -0500

    feat: set up routing with React Router and define initial routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "/dashboard",
+    element: <div>Dashboard</div>,
+  },
+];
